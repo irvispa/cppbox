324aaa2536c66f39a467f73bbf9d4b62320d3f69
Albert <123750816+irvispa@users.noreply.github.com>
2023-12-05 17:43:23 +0300

    Create traverseBinaryTree.cpp

---
+++ traverseBinaryTree.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+void traverseBinaryTree(TreeNode* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    std::cout << root->data << " ";
+
+    traverseBinaryTree(root->left);
+
+    traverseBinaryTree(root->right);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    std::cout << "Prefix traversal of a binary tree: ";
+    traverseBinaryTree(root);
+    std::cout << std::endl;
+
+    delete root->right->right;
+    delete root->right->left;
+    delete root->right;
+    delete root->left->right;
+    delete root->left->left;
+    delete root->left;
+    delete root;
+
+    return 0;
+}
+
